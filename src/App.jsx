ebf196bde31222eba335f8c00091519fbc9c396e
Rainer Simon <rainer.simon@ait.ac.at>
2018-07-18 17:09:45 +0200

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -7,52 +7,16 @@ const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoiYWJvdXRnZW8iLCJhIjoiY2pqcjgzYXl6M29wbjNxcm05MzZqMDJiYSJ9.nIS2lMRvW3KQto_CTt4PPA"
 });
 
-const geojson ={
-  "type": "FeatureCollection",
-  "features": [
-    {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [
-              -8.0859375,
-              50.064191736659104
-            ],
-            [
-              2.4609375,
-              46.800059446787316
-            ],
-            [
-              2.4609375,
-              54.77534585936447
-            ],
-            [
-              -2.4609375,
-              54.77534585936447
-            ],
-            [
-              -10.546875,
-              54.16243396806779
-            ],
-            [
-              -8.0859375,
-              50.064191736659104
-            ]
-          ]
-        ]
-      }
-    }
-  ]
-};
-
 export default class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { places: { "type": "FeatureCollection", "features": [] } }
+    this.state = {
+      places: {
+        "type": "FeatureCollection",
+        "features": []
+      }
+    }
   }
 
   componentDidMount() {
