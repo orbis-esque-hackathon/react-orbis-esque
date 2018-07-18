07db8bce8081c92be210331f876e08e3ac1b7e77
Rainer Simon <rainer.simon@ait.ac.at>
2018-07-18 16:09:16 +0200

    Skeleton package.json + webpack config

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'app': './src/App.jsx',
+    'app.min': './src/App.jsx'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: "[name].js"
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react'],
+        plugins: ['transform-class-properties'],
+        env: {
+          development: {
+            presets: ['react-hmre']
+          }
+        }
+      }
+    },{
+      test: /\.scss$/,
+      loaders: ["style-loader", "css-loader", "sass-loader"]
+    }]
+  },
+  watchOptions: {
+    poll: 1000
+  },
+  devServer: {
+    historyApiFallback: {
+      index: '/'
+    }
+  }
+};
