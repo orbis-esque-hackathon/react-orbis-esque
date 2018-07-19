1efabe1364739c924b271e6cc8f57ede3c8c1d2a
Rainer Simon <rainer.simon@ait.ac.at>
2018-07-19 09:59:01 +0200

    Displaying places/route segments individually

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import ReactMapboxGl, { GeoJSONLayer, Popup } from "react-mapbox-gl";
+import ReactMapboxGl, { Feature, GeoJSONLayer, Layer, Popup } from "react-mapbox-gl";
 import axios from 'axios';
 
 import '../style/app.scss';
@@ -9,36 +9,28 @@ const Map = ReactMapboxGl({
   accessToken: "pk.eyJ1IjoiYWJvdXRnZW8iLCJhIjoiY2pqcjgzYXl6M29wbjNxcm05MzZqMDJiYSJ9.nIS2lMRvW3KQto_CTt4PPA"
 });
 
-const EMPTY_GEOM = {
-  "type": "FeatureCollection",
-  "features": []
-}
-
 export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      places: EMPTY_GEOM, // Places data from GeoJSON
-      routes: EMPTY_GEOM, // Routes data from GeoJSON
-      selected: null,     // Mostly for testing: a feature selected via mouse
-      highlighted: null   // { routes: [], places: [] } to highlight (e.g. after a route calculation)
+      places: [],       // Places data from GeoJSON
+      routes: [],       // Routes data from GeoJSON
+      selected: null,   // Mostly for testing: a feature selected via mouse
+      highlighted: null // { routes: [], places: [] } to highlight (e.g. after a route calculation)
     }
   }
 
   componentDidMount() {
     axios.get('data/places_new_structure.json')
       .then(result => {
-        this.setState({ places: result.data });
+        this.setState({ places: result.data.features });
       });
 
     axios.get('data/routes.json')
       .then(result => {
-        this.setState({ routes: result.data });
+        this.setState({ routes: result.data.features });
       });
-
-    // TODO don't display all as one GeoJSON, but split up features
-    // individually, so we can highlight them through state.
 
     // TODO highlight some routes randomly, for testing...
   }
@@ -64,22 +56,36 @@ export default class App extends Component {
           height: "100vh",
           width: "100vw"
         }}>
-          <GeoJSONLayer
-            data={this.state.places}
-            type
-            circlePaint={{
+          <Layer
+            id="places"
+            type="circle"
+            paint={{
               "circle-color": "#ff0000",
               "circle-opacity": 0.9,
               "circle-radius": 6
             }}
-            circleOnClick={this.onSelectPlace.bind(this)} />
+          >
+            {this.state.places.map(feature =>
+              <Feature
+                key={feature.properties.id}
+                coordinates={feature.geometry.coordinates}
+                onClick={this.onSelectPlace.bind(this)} />
+            )}
+          </Layer>
 
-          <GeoJSONLayer
-            data={this.state.routes}
-            type
-            linePaint={{
+          <Layer
+            id="routes"
+            type="line"
+            paint={{
               "line-color": "#ff0000"
-            }}/>
+            }}
+          >
+            {this.state.routes.map(feature =>
+              <Feature
+                key={feature.properties.id}
+                coordinates={feature.geometry.coordinates} />
+            )}
+          </Layer>
 
           {this.state.selected &&
             <Popup
