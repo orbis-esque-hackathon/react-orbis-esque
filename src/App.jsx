e026e2fc3f8877160c216fff1d310b02bdcdf525
Rainer Simon <rainer.simon@ait.ac.at>
2018-07-19 12:00:05 +0200

    Selecting start & end

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,10 @@ export default class App extends Component {
     this.state = {
       places: [],  // Places data from GeoJSON
       routes: [],  // Routes data from GeoJSON
-      start: null, // Selected start point, if any
-      end: null,   // Selected end point, if any
-      segments: [] // Selected route segments, if any
+      highlighted: { // Selected route (if any)
+        places: [],
+        segments: []
+      }
     }
   }
 
@@ -34,19 +35,20 @@ export default class App extends Component {
       });
   }
 
+  onClick(e) {
+    console.log(e);
+  }
+
   onSelectPlace(e) {
     const feature = (e.features.length > 0) ? e.features[0] : null;
 
-    this.setState({ start: feature });
-    /*
     this.setState(previous => {
-      if (previous.start && !previous.end) // Start set, but no end yet
-        return { end: feature };
-      else // Init new
-        return { start: feature, end: null, segments: [] };
+      if (previous.highlighted.places.length === 0) {
+        return { highlighted: { places: [ feature ] } };
+      } else {
+        return { highlighted: { places: [ previous.highlighted.places[0], feature ] } };
+      }
     });
-
-    console.log(this.state);*/
   }
 
   render() {
@@ -58,7 +60,8 @@ export default class App extends Component {
         containerStyle={{
           height: "100vh",
           width: "100vw"
-        }}>
+        }}
+        onClick={this.onClick.bind(this)}>
           <Layer
             id="places"
             key="places"
@@ -66,7 +69,7 @@ export default class App extends Component {
             paint={{
               "circle-color": "red",
               "circle-opacity": 0.9,
-              "circle-radius": 6
+              "circle-radius": 10
             }}>
             {this.state.places.map(feature =>
               <Feature
@@ -91,30 +94,31 @@ export default class App extends Component {
             )}
           </Layer>
 
-          <Layer
-            id="selected_path"
-            type="line"
-            paint={{ "line-color": "green" }}>
-            {this.state.segments.map(feature =>
-              <Feature
-                key={feature.properties.id}
-                coordinates={feature.geometry.coordinates} />
-            )}
-          </Layer>
-
-          {this.state.start &&
+          {this.state.highlighted.routes &&
             <Layer
-              id="selected_places"
-              type="circle"
-              paint={{ "circle-color": "blue" }}>
-
-              <Feature coordinates={this.state.start.geometry.coordinates} />
-
-              {this.state.end &&
-                <Feature coordinates={this.state.end.geometry.coordinates} />
-              }
+              id="selected_path"
+              type="line"
+              paint={{ "line-color": "green" }}>
+              {this.state.highlighted.segments.map(feature =>
+                <Feature
+                  key={feature.properties.id}
+                  coordinates={feature.geometry.coordinates} />
+              )}
             </Layer>
           }
+
+          <Layer
+            id="selected_places"
+            type="circle"
+            paint={{
+              "circle-color": "blue",
+              "circle-opacity": 0.9,
+              "circle-radius": 6
+            }}>
+            {this.state.highlighted.places.map(feature =>
+              <Feature coordinates={feature.geometry.coordinates} />
+            )}
+          </Layer>
       </Map>
     )
   }
